96272bd4c74358299823fe7a580d66f44b59b83a
Albans1 <63380956+Albans1@users.noreply.github.com>
2021-10-22 16:19:40 +0700

    Post Model

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,41 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Yuji Itadori",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
-            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
-            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus soluta laudantium, adipisci, culpa esse?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Roby Oboy",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
-            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
-            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. 
-            Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, 
-            tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a 
-            rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus 
-             soluta laudantium, adipisci, culpa esse?"
-        ],
-        ];
+    use HasFactory;
 
-        public static function all()
-        {
-            return collect(self::$blog_posts);
-        }
-
-        public static function find($slug)
-        {
-            $posts = static::all();
-            return $posts->firstWhere('slug', $slug);
-        }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -1,12 +1,10 @@
-
-
 @extends('layouts.main')
 
 @section('container')
     <article>
-        <h2>{{ $post["title"] }}</h2>
-        <h5>{{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+        <h2>{{ $post->title }}</h2>
+        <h5>{{ $post->author }}</h5>
+        {!! $post->body !!}
     </article>
 
     <a href="/blog">Back to Post</a>
