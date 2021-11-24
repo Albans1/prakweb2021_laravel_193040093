dbde509199d64363b9e131c3dbc7b66f74a43db1
Albans1 <63380956+Albans1@users.noreply.github.com>
2021-11-24 12:23:25 +0700

    Create Post Form

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     //protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
@@ -50,5 +53,14 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
